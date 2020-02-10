ca84bef0297d7aa47a92ebe41da43c526ccb7c8d
Maurizio Brioschi <mbrioschi@mediawave.de>
2020-02-10 11:58:42 +0100

    star feature
    srai feature

--- src/Category.php
+++ src/Category.php
@@ -7,6 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Ridesoft\AIML;
 
 class Category
@@ -15,6 +16,10 @@ class Category
     protected $pattern;
     /** @var string */
     protected $template;
+    /** @var bool */
+    protected $isTemplateSrai;
+    /** @var */
+    protected $stars = [];
 
     public function __construct(string $pattern, string $template)
     {
@@ -27,14 +32,75 @@ class Category
      */
     public function getPattern()
     {
+        if ($this->isTemplateSrai()) {
+            $this->pattern = str_replace(
+                '<srai>',
+                '',
+                str_replace(
+                    '</srai>',
+                    '',
+                    $this->pattern
+                )
+            );
+            $this->pattern = str_replace("<star/>", '*', $this->pattern);
+            preg_match_all('/<star index="\d"\/>/', $this->pattern, $output);
+            foreach ($output as $value) {
+                $this->pattern = str_replace($value, '*', $this->pattern);
+            }
+        }
         return $this->pattern;
     }
 
     /**
+     * Return the template element or the patter when the category is a srai
+     *
+     * @param string ...$stars
      * @return string
      */
-    public function getTemplate()
+    public function getTemplate(string ...$stars): string
     {
+        if ($this->isTemplateSrai()) {
+            return $this->getPattern();
+        }
+        $matches = preg_match_all('/\*/m', $this->getPattern(), $arguments);
+        if ($matches > 0 && count($stars) == $matches) {
+            for ($i = 0; $i < $matches; $i++) {
+                $this->stars[$i + 1] = $stars[$i];
+            }
+        }
+        if ($matches === 1) {
+            $this->template = str_replace("<star/>", $this->stars[1], $this->template);
+        } elseif ($matches > 1) {
+            foreach ($this->stars as $key => $star) {
+                $this->template = str_replace("<star index=\"" . $key . "\"/>", $star, $this->template);
+            }
+        }
         return $this->template;
+    }
+
+    /**
+     * @param array $stars
+     * @return array
+     */
+    public function getStars(...$stars): array
+    {
+        $matches = preg_match_all('/\*/m', $this->pattern, $arguments);
+        if ($matches > 0 && count($stars) == $matches) {
+            for ($i = 0; $i < $matches; $i++) {
+                $this->stars[$i + 1] = $stars[$i];
+            }
+        }
+        return $this->stars;
+    }
+
+    protected function isTemplateSrai(): bool
+    {
+        $this->isTemplateSrai = false;
+        $found = preg_match('/<srai>(.*?)<\/srai>/', $this->template, $output);
+        if ($found) {
+            $this->isTemplateSrai = true;
+            $this->pattern = $output[1];
+        }
+        return $this->isTemplateSrai;
     }
 }
--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -6,27 +6,106 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Rideaoft\AIML\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Ridesoft\AIML\Category;
 
 class CategoryTest extends TestCase
 {
-    /** @var \Ridesoft\AIML\Category */
-    protected $category;
-
-    protected function setUp(): void
-    {
-        $this->category = new \Ridesoft\AIML\Category('pattern', 'template');
-    }
-
     public function testGetPattern()
     {
-        $this->assertEquals('pattern', $this->category->getPattern());
+        $category = new Category('pattern', 'template');
+        $this->assertEquals('pattern', $category->getPattern());
     }
 
     public function testGetTemplate()
     {
-        $this->assertEquals('template', $this->category->getTemplate());
+        $category = new Category('pattern', 'template');
+        $this->assertEquals('template', $category->getTemplate());
+
+        $category = new Category(
+            '* is a *.',
+            "<star index=\"1\"/> is a <star index=\"2\"/>?"
+        );
+        $this->assertEquals(
+            'Mauri is a rock star?',
+            $category->getTemplate('Mauri', 'rock star')
+        );
+
+        $category = new Category(
+            'I love *',
+            "I love <star/>"
+        );
+        $this->assertEquals(
+            'I love pussy',
+            $category->getTemplate('pussy')
+        );
+    }
+
+    public function testGetStars()
+    {
+        $category = new Category(
+            'A * is a *.',
+            "When a <star index=\"1\"/>is not a<star index=\"2\"/>?"
+        );
+        $stars = $category->getStars('Mauri', 'snowboarder');
+        $this->assertEquals(2, count($stars));
+        $this->assertEquals('Mauri', $stars[1]);
+        $this->assertEquals('snowboarder', $stars[2]);
+
+        $category = new Category(
+            'I love *',
+            "I love <star/>"
+        );
+        $stars = $category->getStars('Pussy');
+        $this->assertEquals(1, count($stars));
+        $this->assertEquals('Pussy', $stars[1]);
+    }
+
+    public function testSrai()
+    {
+        $category = new Category(
+            'DO YOU KNOW WHO * IS?',
+            "<srai>WHO IS <star/>?</srai>"
+        );
+        $this->assertEquals(
+            'WHO IS *?',
+            $category->getPattern()
+        );
+
+        $category = new Category(
+            'BYE *',
+            "<srai>BYE</srai>"
+        );
+        $this->assertEquals(
+            'BYE',
+            $category->getPattern()
+        );
+
+        $category = new Category(
+            'I like * and *',
+            "<srai>I like <star index=\"1\"/>,<star index=\"2\"/></srai>"
+        );
+        $this->assertEquals(
+            'I like *,*',
+            $category->getPattern()
+        );
+
+        $category = new Category(
+            'My family is done by *,* and *',
+            "<srai>My family has three elements: <star index=\"1\"/>, <star index=\"2\"/> and <star index=\"3\"/></srai>"
+        );
+        $this->assertEquals(
+            'My family has three elements: *, * and *',
+            $category->getPattern()
+        );
+        $this->assertEquals(
+            'My family has three elements: *, * and *',
+            $category->getTemplate()
+        );
+
+
     }
 }
